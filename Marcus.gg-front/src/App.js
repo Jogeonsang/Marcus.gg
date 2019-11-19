484d620fbf9fc08a5ca6f14b74eb59d68195affb
Jogeonsang <rhcn258@gmail.com>
2019-11-19 23:52:19 +0900

    Front Initial Setting
    create-react-app & reset css

---
+++ Marcus.gg-front/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import GlobalStyles from "./GlobalStyles";
+
+const App = () => {
+  return (
+      <>
+        <GlobalStyles/>
+      </>
+  )
+}
+
+export default App;
