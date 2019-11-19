d00e5320d6ee3fa411f548db1739a672e707cc4c
Jogeonsang <rhcn258@gmail.com>
2019-11-19 21:12:50 +0900

    Marcus.gg create API

--- Marcus.gg-server/handler.js
+++ Marcus.gg-server/handler.js
@@ -1,18 +1,99 @@
 'use strict';
+const axios = require('axios');
+const { api_key } = require('./../Marcus.gg-server/src/config/config')
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+// static Data
+module.exports.LeagueOfLegendItem = async event => {
+    return axios
+        .get('http://ddragon.leagueoflegends.com/cdn/6.24.1/data/ko_KR/item.json')
+        .then(response => {
+            return {
+                statusCode: 200,
+                body: JSON.stringify(
+                    {
+                        data: response.data
+                    },
+                )
+            };
+        });
+};
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.LeagueOfLegendChampion = async event => {
+    return axios
+        .get('http://ddragon.leagueoflegends.com/cdn/6.24.1/data/ko_KR/champion.json')
+        .then(response => {
+            return {
+                statusCode: 200,
+                body: JSON.stringify(
+                    {
+                        data: response.data
+                    },
+                )
+            }
+        })
+}
+
+// api
+module.exports.SummonerInfo = async event => {
+    const summonerName = event.pathParameters.summonerName;
+    return axios
+        .get(`https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${api_key}`)
+        .then(response => {
+            return {
+                statusCode: 200,
+                body: JSON.stringify(
+                    {
+                        data: response.data
+                    },
+                )
+            }
+        })
+};
+
+module.exports.SummonerLeagueInfo = async event => {
+    const summonerId = event.pathParameters.summonerId;
+    return axios
+        .get(`https://kr.api.riotgames.com/lol/league/v4/positions/by-summoner/${summonerId}?api_key=${api_key}`)
+        .then(response => {
+            return {
+                statusCode: 200,
+                body: JSON.stringify(
+                    {
+                        data: response.data
+                    },
+                )
+            }
+        })
+};
+
+module.exports.SummonerGameList = async event => {
+    const accountId = event.pathParameters.accountId;
+    return axios
+        .get(`https://kr.api.riotgames.com/lol/match/v4/matchlists/by-account/${accountId}?endIndex=10&api_key=${api_key}`)
+        .then(response => {
+            return {
+                statusCode: 200,
+                body: JSON.stringify(
+                    {
+                        data: response.data
+                    },
+                )
+            }
+        })
+};
+
+module.exports.SummonerDetailGameInfo = async event => {
+    const gameId = event.pathParameters.gameId;
+    return axios
+        .get(`https://kr.api.riotgames.com/lol/match/v4/matches/${gameId}?api_key=${api_key}`)
+        .then(response => {
+            return {
+                statusCode: 200,
+                body: JSON.stringify(
+                    {
+                        data: response.data,
+                    },
+                )
+            }
+        })
 };
---
+++ README.md
@@ -0,0 +1,9 @@
+## RIOT API를 이용한 League of Legend 전적검색 사이트
+
+### API TODO
+- [O] Summoner Search API
+- [O] Summoner League Info API
+- [O] Summoner GameList API
+- [O] Summoner DetailGameList API
+- [O] LeagueOfLegend Items API
+- [O] LeagueOfLegend Champions API 
