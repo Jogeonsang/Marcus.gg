ee81c822f7b10129e4cd9c26495b35b8bdbeb390
Jogeonsang <rhcn258@gmail.com>
2020-01-20 23:18:18 +0900

    RecentPlayChampion Part.1

---
+++ Marcus.gg-front/src/pages/summonerSearch/recentChampionList/recentChampion/recentChampion.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import styled from 'styled-components';
+import {useChampion} from "../../../../commons/context";
+
+const RecentChampion = ({data}) => {
+  const [champion]= useChampion(data.champion.champion);
+  return (
+    <RecentChampionWrapper>
+      <LeftBox>
+        <ChampionIcon src={`http://ddragon.leagueoflegends.com/cdn/10.1.1/img/champion/${champion.id}.png`} />
+        <ChampionInfo>
+          <ChampionName>
+            {champion.name}
+          </ChampionName>
+          <ChampionPlayCount>
+            {data.count}
+          </ChampionPlayCount>
+        </ChampionInfo>
+      </LeftBox>
+    </RecentChampionWrapper>
+  )
+};
+
+export default  RecentChampion
+
+const RecentChampionWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 0;
+`;
+const LeftBox = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const ChampionIcon = styled.img`
+  width: 36px;
+  height: 36px;
+  -webkit-border-radius: 50%;-moz-border-radius: 50%;border-radius: 50%;
+  margin-right: 10px;
+`;
+
+const ChampionInfo = styled.div`
+  
+`;
+
+const ChampionName = styled.div`
+  font-size: 13px;
+  color: #FFF;
+  padding-bottom: 5px;
+`;
+
+const ChampionPlayCount = styled.div`
+  font-size: 13px;
+  color: rgb(137, 160, 181);
+`;
---
+++ Marcus.gg-front/src/pages/summonerSearch/recentChampionList/recentChampionList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+import RecentChampion from "./recentChampion/recentChampion";
+
+const RecentChampionList = ({recentChampion}) => {
+
+  return (
+    <RecentChampionListWrapper>
+      <Title>
+        시즌 10 챔피언 통계
+      </Title>
+      {recentChampion.data.map(each => (
+          <RecentChampion data={each}/>
+        )
+      )}
+    </RecentChampionListWrapper>
+  )
+};
+
+export default RecentChampionList
+
+const RecentChampionListWrapper = styled.div`
+  width: 100%;
+`;
+const Title = styled.div`
+  font-size: 14px;
+  color: #FFFFFF;
+  margin-bottom: 15px;
+`;
