2eb53edefe3f7454c09fdcd9ffe2e32a38589a13
Jogeonsang <rhcn258@gmail.com>
2020-02-14 00:20:40 +0900

    recentSummary 완료

--- Marcus.gg-front/src/pages/summonerSearch/recentRecord/recentSummary/recentSummary.js
+++ Marcus.gg-front/src/pages/summonerSearch/recentRecord/recentSummary/recentSummary.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import {getKDA} from "../../../../commons/calculationUtil";
+import {getKDA, getVictoryRate} from "../../../../commons/calculationUtil";
 
 const RecentSummary = ({data}) => {
 
@@ -14,16 +14,23 @@ const RecentSummary = ({data}) => {
     }
     return obj
   }, {count: 0, kills: 0, deaths: 0, assists: 0, wins: 0});
-  const {count, kills, deaths, assists, wins} = recentSummary;
+  let {count, kills, deaths, assists, wins} = recentSummary;
+  kills = kills / count;
+  deaths = deaths / count
+  assists = assists / count;
   return (
     <RecentSummaryWrapper>
-      <div>
-        <WinLose>{`${wins}W ${count-wins}L`}</WinLose>
-        <LastGame>{`마지막 게임 ${count}`}</LastGame>
+      <div style={{marginRight: '1.5rem'}}>
+        <RecentRecordWins>{`${wins}W ${count-wins}L`}</RecentRecordWins>
+        <ColumnBottom>{`마지막 게임 ${count}`}</ColumnBottom>
       </div>
       <div>
-        <KdaAverage>{`${getKDA(recentSummary)} KDA`}</KdaAverage>
+        <KdaAverage KDA={getKDA(recentSummary)}>{`${getKDA(recentSummary)} KDA`}</KdaAverage>
+        <ColumnBottom>{`${kills} / ${deaths} / ${assists}`}</ColumnBottom>
       </div>
+      <ProgressbarWrapper>
+        <Progressbar Average={getVictoryRate(wins, count-wins)}/>
+      </ProgressbarWrapper>
     </RecentSummaryWrapper>
   )
 };
@@ -35,7 +42,7 @@ const RecentSummaryWrapper = styled.div`
   flex-wrap: wrap;
 `;
 
-const WinLose = styled.p`
+const RecentRecordWins = styled.p`
   color: #E5EBEF;
   padding-top: 2px;
   font-size: 0.875rem;
@@ -44,21 +51,12 @@ const WinLose = styled.p`
   margin: 0;
 `;
 
-const LastGame = styled.p`
-  padding-top: 2px;
-  font-size: 0.75rem;
-  font-weight: normal;
-  font-style: normal;
-  color: rgb(137, 160, 181);
-  margin: 0px;
-`;
-
 const KdaAverage = styled.p`
   padding-top: 2px;
   font-size: 0.875rem;
-    font-weight: normal;
-    font-style: normal;
-    margin: 0px;
+  font-weight: normal;
+  font-style: normal;
+  margin: 0px;
   color: ${props => {
   if (props.KDA <= 1) return 'rgb(130, 135, 144)'
   else if (props.KDA <= 2) return 'rgb(151, 141, 135)'
@@ -67,4 +65,33 @@ const KdaAverage = styled.p`
   else if (props.KDA <= 10) return 'rgb(255, 148, 23)'
   else return 'rgb(255, 148, 23)'
 }};
+`;
+
+const ColumnBottom = styled.p`
+  padding-top: 7px;
+  font-size: 0.75rem;
+  font-weight: normal;
+  font-style: normal;
+  color: rgb(137, 160, 181);
+  margin: 0px;
+`;
+
+const ProgressbarWrapper = styled.div`
+  position: relative;
+  height: 2px;
+  margin-top: 0.75em;
+  width: 100%;
+  background: rgb(52, 63, 87);
+  border-radius: 3px;
+`;
+
+const Progressbar = styled.div`
+  content: "";
+  height: 2px;
+  left: 0px;
+  position: absolute;
+  top: 0px;
+  width: ${props => props.Average}%;
+  border-radius: 6px;
+  background: rgb(119, 188, 243);
 `;
